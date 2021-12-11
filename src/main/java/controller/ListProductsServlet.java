2fa819732cc01e504e9fadcbc6225ad3ca7c02f2
lucasmp <lucasmatiasperez@gmail.com>
2021-12-11 11:10:45 -0300

    Se crearon el servlet para listar los productos de un usuario particular y el service correspondiente 'ProductService' con el método list()

--- src/main/java/controller/ListProductsServlet.java
+++ src/main/java/controller/ListProductsServlet.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.util.List;
 
+import dao.DAOFactory;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
@@ -14,10 +15,10 @@ import model.Producto;
 import model.Usuario;
 import services.ProductService;
 
-@WebServlet("usuario/index.do"/*Definir la ruta (Gonza)*/)
+@WebServlet("/listProducts.do")
 public class ListProductsServlet extends HttpServlet implements Servlet {
 
-	private static final long serialVersionUID = 7256385346721236583L;
+	private static final long serialVersionUID = 6726512261455231345L;
 	private ProductService productService;
 	
 	@Override
@@ -30,13 +31,12 @@ public class ListProductsServlet extends HttpServlet implements Servlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		Usuario usuario = (Usuario) req.getSession().getAttribute("usuario");
+//		Usuario usuario = DAOFactory.getUsuarioDAO().findUsuarioByNombre(req.getParameter("nombre"));
 		
 		List<Producto> productos = productService.list(usuario);
 		req.setAttribute("productos", productos);
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/usuario/index.do"/*¿?*/);
+
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
 		dispatcher.forward(req, resp);
-		
 	}
-	
 }
--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,89 +1,43 @@
 package services;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import dao.AtraccionesDAO;
 import dao.DAOFactory;
 import dao.ItinerarioDAO;
 import dao.PromocionDAO;
-import model.Atraccion;
-import model.ComparadorClase;
-import model.ComparadorCosto;
-import model.ComparadorProducto;
-import model.ComparadorTiempo;
-import model.ComparadorTipoPreferido;
 import model.Itinerario;
 import model.Producto;
-import model.Promocion;
-import model.Tipo;
+import model.SecretariaTurismo;
 import model.Usuario;
 
 public class ProductService {
-	private Map<String, Atraccion> atracciones = new HashMap<String, Atraccion>();
-	private List<Promocion> promociones = new LinkedList<Promocion>();
-	private Map<String, Producto> productos = new HashMap<String, Producto>();
+
 	private AtraccionesDAO gestorAtracciones = DAOFactory.getAtraccionesDAO();
 	private PromocionDAO gestorPromociones = DAOFactory.getPromocionDAO();
 	private ItinerarioDAO gestorItinerarios = DAOFactory.getItinerarioDAO();
-
+	private SecretariaTurismo st;
 	
 	public ProductService() {
-		this.atracciones = this.gestorAtracciones.findAllAtracciones();
-		this.promociones = this.gestorPromociones.findAll(atracciones);
-		setProductos();
-	}
-
-	private void setProductos() {
-		this.productos.putAll(this.atracciones);
-		for (Promocion promo : this.promociones)
-			this.productos.put(promo.getNombre(), promo);
+		this.st = new SecretariaTurismo(this.gestorAtracciones.findAllAtracciones(),
+				this.gestorPromociones.findAll(this.gestorAtracciones.findAllAtracciones()));
 	}
 	
-	private ComparadorProducto generarComparadorProducto(Tipo tipoPreferido) {
-		List<Comparator<Producto>> comparadores = new LinkedList<Comparator<Producto>>();
-		comparadores.add(new ComparadorTipoPreferido(tipoPreferido));
-		comparadores.add(new ComparadorClase());
-		comparadores.add(new ComparadorCosto());
-		comparadores.add(new ComparadorTiempo());
-
-		return (new ComparadorProducto(comparadores));
-	}
-	
-	//Este método debería ser responsabilidad del usuario --> Refactorizar class Usuario
-	private boolean atraccionComprada(Producto p, List<Producto> productosItinerario) {
-		for (Producto producto : productosItinerario) {
-			if (p.equals(producto)) return true;
-			if (producto.esPromocion()) {
-				for (Atraccion atr : producto.getAtracciones())
-					if (p.equals(atr)) return true;
-			}
-		}
-		return false;
-	}
-	
-	private List<Producto> getProductosParaUsuario(Usuario usuario) {
-		List<Producto> resultado = new LinkedList<Producto>(this.productos.values());
-		Collections.sort(resultado, generarComparadorProducto(usuario.getTipoPreferido()));
-		return resultado;
-	}
-	
+	//Refactorizar nombre de variables!
 	public List<Producto> list(Usuario usuario) {
+			
 		List<Producto> productos = new LinkedList<Producto>();
-		Itinerario itinerario = this.gestorItinerarios.findItinerarioByUsuario(usuario.getNombre(), this.productos);
+		Itinerario itinerario = this.gestorItinerarios.findItinerarioByUsuario(usuario.getNombre(), st.getProductos());
 		// 1° crear NullItinerario.java
 		if (itinerario == null) itinerario = new Itinerario(usuario.getNombre());
 		
-		Iterator<Producto> itr = this.getProductosParaUsuario(usuario).iterator();
+		Iterator<Producto> itr = st.getProductosParaUsuario(usuario).iterator();
 		
 		while (itr.hasNext()) {
 			Producto producto = itr.next();
-			if (!atraccionComprada(producto, itinerario.getProductos())) {
+			if (!st.atraccionComprada(producto, itinerario.getProductos())) {
 				productos.add(producto);
 			}
 		}
